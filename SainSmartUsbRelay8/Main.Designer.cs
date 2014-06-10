37e8af7da1b30a3891498488124e7bf129ac4bf2
Johnny Wiedmann <john.wiedmann@gmail.com>
2014-06-10 12:07:23 -0400

    First Cut
    Project not in a pretty state.
    Making the base classes for the SainSmart Relay 8.
    Need to fix up the UI to work with the new base relay classes.

---
+++ SainSmartUsbRelay8/Main.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace SainSmartUsbRelay8
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.gbConnect = new System.Windows.Forms.GroupBox();
+            this.tbDeviceCount = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblDeviceCount = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.flpRelays = new System.Windows.Forms.FlowLayoutPanel();
+            this.gbConnect.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // gbConnect
+            // 
+            this.gbConnect.Controls.Add(this.tbDeviceCount);
+            this.gbConnect.Controls.Add(this.btnConnect);
+            this.gbConnect.Controls.Add(this.lblDeviceCount);
+            this.gbConnect.Location = new System.Drawing.Point(13, 22);
+            this.gbConnect.Name = "gbConnect";
+            this.gbConnect.Size = new System.Drawing.Size(146, 85);
+            this.gbConnect.TabIndex = 1;
+            this.gbConnect.TabStop = false;
+            this.gbConnect.Text = "Connect";
+            // 
+            // tbDeviceCount
+            // 
+            this.tbDeviceCount.Location = new System.Drawing.Point(93, 50);
+            this.tbDeviceCount.Name = "tbDeviceCount";
+            this.tbDeviceCount.ReadOnly = true;
+            this.tbDeviceCount.Size = new System.Drawing.Size(36, 20);
+            this.tbDeviceCount.TabIndex = 2;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 19);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(117, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // lblDeviceCount
+            // 
+            this.lblDeviceCount.AutoSize = true;
+            this.lblDeviceCount.Location = new System.Drawing.Point(15, 53);
+            this.lblDeviceCount.Name = "lblDeviceCount";
+            this.lblDeviceCount.Size = new System.Drawing.Size(72, 13);
+            this.lblDeviceCount.TabIndex = 0;
+            this.lblDeviceCount.Text = "Device Count";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.flpRelays);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(188, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(165, 266);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "USB Relays";
+            // 
+            // flpRelays
+            // 
+            this.flpRelays.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flpRelays.Location = new System.Drawing.Point(6, 53);
+            this.flpRelays.Name = "flpRelays";
+            this.flpRelays.Size = new System.Drawing.Size(153, 207);
+            this.flpRelays.TabIndex = 1;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 300);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbConnect);
+            this.Name = "frmMain";
+            this.Text = "s";
+            this.gbConnect.ResumeLayout(false);
+            this.gbConnect.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox gbConnect;
+        private System.Windows.Forms.TextBox tbDeviceCount;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblDeviceCount;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.FlowLayoutPanel flpRelays;
+    }
+}
+
---
+++ SainSmartUsbRelay8/UsbRelay8Driver.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FTD2XX_NET;
+
+// Do List
+// 1) Add the ability to add relay device by serial number
+
+
+namespace UsbRelay8Driver
+{
+    #region Exceptions
+    class UsbRelayDeviceNotFoundException : Exception
+    {
+        public override string Message
+        {
+	        get { return "SainSmart USB relay not found."; }
+        }
+    }
+
+    class UsbRelayConfigurationException : Exception
+    {
+        public override string Message
+        {
+            get { return "SainSmart USB relay configuration error."; }
+        }
+    }
+
+    class UsbRelayInvalidRelayException : Exception
+    {
+        public override string Message
+        {
+	        get { return "SainSmart USB relay invalid relay number."; }
+        }
+    }
+
+    class UsbRelayReadException : Exception
+    {
+        public override string Message
+        {
+            get { return "SainSmart USB relay read error."; }
+        }
+    }
+
+    class UsbRelayWriteException : Exception
+    {
+        public override string Message
+        {
+            get { return "SainSmart USB relay write error."; }
+        }
+    }
+    #endregion // Exceptions
+
+    /// <summary>
+    /// SainSmart USB Relay 8 Device Class
+    /// This class handles the read/writes to a particular SainSmart USB
+    /// relay 8 device.  Because of the nature of the writes/reads to the
+    /// device with 8 relays.  It may work with device with less than
+    /// 8 relays, but it has not been tested.
+    /// </summary>
+    class UsbRelay8
+    {
+        #region Constants
+        const int BaudRate = 921600;
+        const byte BitMask = 0xff;          // 8-bit mask
+        const byte BitMode = FTDI.FT_BIT_MODES.FT_BIT_MODE_SYNC_BITBANG;
+        const int RelayCount = 8;           // Maximum number of relays supported by device
+        #endregion // Constants
+
+        #region Private Variables
+        uint _deviceIndex;
+        FTDI _device;
+        FTDI.FT_STATUS _status;
+
+        // FTD2 driver requires byte arrays, only a byte is needed for device
+        byte[] _writeBuffer = new byte[2];
+        byte[] _readBuffer = new byte[2];
+
+        #endregion // Private Variables
+
+        #region Public Properties
+        /// <summary>
+        /// Relay Device ID
+        /// </summary>
+        public uint DeviceIndex
+        {
+            get {return _deviceIndex; }
+            private set;
+        }
+
+        #endregion // Public Properties
+
+        #region Constuctors
+        /// <summary>
+        /// Create a USB Relay Device
+        /// </summary>
+        /// <param name="deviceIndex">Device Index from FTDI driver</param>
+        public UsbRelay8( uint deviceIndex )
+        {
+
+            // Open the relay device
+            _device = new FTDI();
+            _deviceIndex = deviceIndex;
+            _status = _device.OpenByIndex(_deviceIndex);
+            if (_status != FTDI.FT_STATUS.FT_OK)
+            {
+                throw new UsbRelayDeviceNotFoundException();
+            }
+
+            // Set the baud rate
+            _status = _device.SetBaudRate(BaudRate);
+            if (_status != FTDI.FT_STATUS.FT_OK)
+            {
+                throw new UsbRelayConfigurationException();
+            }
+            
+            // Set the bit mode
+            _status = _device.SetBitMode(BitMask, BitMode);
+            if (_status != FTDI.FT_STATUS.FT_OK)
+            {
+                throw new UsbRelayConfigurationException();
+            }
+        }
+
+        #endregion  // Constructors
+
+        #region Public Methods
+        /// <summary>
+        /// Get the relay values
+        /// </summary>
+        /// <returns></returns>
+        public byte GetRelayValues()
+        {
+            // Clear the read buffer
+            Array.Clear(_readBuffer, 0, _readBuffer.Length);
+
+            // Read the relay values
+            uint readBytes = 1;
+            uint bytesRead = 0;
+            _status = _device.Read(_readBuffer, readBytes, ref bytesRead);
+            if (_status != FTDI.FT_STATUS.FT_OK || bytesRead != 1)
+            {
+                throw new UsbRelayReadException();
+            }
+
+            return _readBuffer[0];
+        }
+
+        /// <summary>
+        /// Set an individual relay value.
+        /// </summary>
+        /// <param name="relay">relay index, zero based</param>
+        /// <param name="value">relay value</param>
+        public void SetRelay(int relay, bool value)
+        {
+            // Relay sanity check
+            if (relay < 0 || relay > RelayCount - 1)
+            {
+                throw new UsbRelayInvalidRelayException();
+            }
+            
+            // Get the current values
+            byte newValue = GetRelayValues();
+
+            // Please note, that this only works for devices with up to 8 relays 
+            // note: C# doesn't have a power operator, have to use a Math a method call. 
+            byte bitValue = (byte) Math.Pow(2,relay);
+            if (true == value)
+            {
+                // Setting a relay value, or it in.
+                newValue |= bitValue;
+            }
+            else
+            {
+                // Clearing a relay value, and it out
+                newValue &= (byte) ~bitValue;
+            }
+            _writeBuffer[0] = newValue;
+
+            // Write the value
+            uint bytesWritten = 0;
+            _status = _device.Write(_writeBuffer, 1, ref bytesWritten);
+            if (_status != FTDI.FT_STATUS.FT_OK || bytesWritten != 1)
+            {
+                throw new UsbRelayWriteException();
+            }
+        }
+
+        /// <summary>
+        /// Set all the relay values, each relay is a bit
+        /// </summary>
+        /// <param name="values">combined relay values</param>
+        public void SetRelays(byte values)
+        {
+            // Set the write buffer with the values
+            _writeBuffer[0] = values;
+
+            // Write the value
+            uint bytesWritten = 0;
+            _status = _device.Write(_writeBuffer, 1, ref bytesWritten);
+            if (_status != FTDI.FT_STATUS.FT_OK || bytesWritten != 1)
+            {
+                throw new UsbRelayWriteException();
+            }
+        }
+
+        #endregion  // Public Methods
+    }
+
+
+    /// <summary>
+    /// USB Relays Class.  
+    /// This class holds a collection of relay devices and handles the setting
+    /// of relays by using DeviceName:RelayNumber or assigned names for individual
+    /// DeviceName:RelayNumber pairs.
+    /// </summary>
+    class UsbRelays
+    {
+        #region Constants
+        #endregion      // Constants
+       
+        #region Private Variables
+        private bool _Opened = false;
+        private int _DeviceCount = 0;
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Device Count
+        /// </summary>
+        public int DeviceCount
+        {
+            get { return GetDeviceCount(); }
+            private set;
+        }
+
+        /// <summary>
+        /// Return the device count
+        /// </summary>
+        /// <returns></returns>
+        private int GetDeviceCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion      // Constants
+
+    }
+}
